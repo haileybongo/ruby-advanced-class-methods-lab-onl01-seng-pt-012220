196a2dbea1794280b220e6d5b1213441846ea0db
Hailey Bongo <haileyebongo@gmail.com>
2020-02-13 21:33:54 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -9,5 +9,48 @@ class Song
   def save
     self.class.all << self
   end
+  
+  def self.create
+    track = self.new 
+    track.save
+    track
+  end
+  
+  def self.new_by_name(title)
+    track = self.new 
+    track.save
+    track.name = title 
+    track 
+  end
+  
+  def self.create_by_name(title)
+     track = self.new 
+    track.save
+    track.name = title 
+    track 
+  end
+    
+  def self.find_by_name(title)
+    @@all.find{|song| song.name == title}
+  end
+  
+  def self.find_or_create_by_name(title)
+    if self.find_by_name(title) != NIL
+      self.find_by_name(title)
+    else 
+      self.create_by_name(title)
+    end
+  end
+  
+  def self.alphabetical
+    @@all.sort_by(&:name)
+  end
+  
+  def self.new_from_filename(file)
+    file_array = file.split(" "||".")
+    file_array.collect do |i|
+      if i == "-" || "." || "mp3"
+        i.delete 
+      
 
 end
